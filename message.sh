c788d8b74fe20bc4fcfca05194a54f1d67e61430
Filip Troníček <filip@gitpod.io>
2025-01-21 12:46:55 +0000

    jhmm
    Tool: gitpod/ft-org-git4ab8821b53.preview.gitpod-dev.com (modified by pre-commit) (modified by pre-commit)

--- message.sh
+++ message.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-COMMIT_MSG_FILE=$1
+# pre-commit automatically passes the commit message file path as $1
+COMMIT_MSG_FILE="$1"
 
-# Append the text to the commit message
-echo "$(cat $COMMIT_MSG_FILE) (modified by pre-commit)" > $COMMIT_MSG_FILE
+if [ ! -f "$COMMIT_MSG_FILE" ]; then
+    echo "Error: Commit message file not found at $COMMIT_MSG_FILE"
+    exit 1
+fi
+
+# Read and modify the commit message
+echo "$(cat "$COMMIT_MSG_FILE") (modified by pre-commit)" > "$COMMIT_MSG_FILE"
